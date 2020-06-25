30018698b7550a009865aa803316c9f9a8a1154d
Larissa <larissasilvavarjao@gmail.com>
2020-06-25 18:09:43 -0300

    feat: include providers to persist data into app

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
 import 'package:sintomed_app/routes.dart';
+import 'package:sintomed_app/src/stores/syntom/syntom_store.dart';
 import 'package:sintomed_app/src/ui/screens/splash_screen.dart';
 import 'package:sintomed_app/src/utils/colors.dart';
 
@@ -9,24 +11,30 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+  final SyntomStore _syntomStore = SyntomStore();
+
   @override
   Widget build(BuildContext context) {
-    return Observer(
-      builder: (context) {
-        return MaterialApp(
-          title: 'Sintomed',
-          routes: Routes.routes,
-          theme: ThemeData(
-            primaryColor: kPrimaryColor.shade900,
-            fontFamily: 'Montserrat',
-            primarySwatch: kPrimaryColor,
-            backgroundColor: Colors.white,
-            scaffoldBackgroundColor: Colors.white,
-          ),
-          home: SplashScreen(),
-        );
-      },
+    return MultiProvider(
+      providers: [
+        Provider<SyntomStore>.value(value: _syntomStore),
+      ],
+      child: Observer(
+        builder: (context) {
+          return MaterialApp(
+            title: 'Sintomed',
+            routes: Routes.routes,
+            theme: ThemeData(
+              primaryColor: kPrimaryColor.shade900,
+              fontFamily: 'Montserrat',
+              primarySwatch: kPrimaryColor,
+              backgroundColor: Colors.white,
+              scaffoldBackgroundColor: Colors.white,
+            ),
+            home: SplashScreen(),
+          );
+        },
+      ),
     );
   }
 }
--- lib/src/ui/screens/home_screen.dart
+++ lib/src/ui/screens/home_screen.dart
@@ -1,39 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:sintomed_app/src/stores/syntom_type/syntom_type_store.dart';
+import 'package:provider/provider.dart';
+import 'package:sintomed_app/src/stores/syntom/syntom_store.dart';
 import 'package:sintomed_app/src/ui/widgets/loading_widget.dart';
 
 class HomeScreen extends StatefulWidget {
-  final SyntomTypeStore store = SyntomTypeStore();
-
   @override
   _HomeScreenState createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  SyntomStore _syntomStore;
+
   @override
   void initState() {
     super.initState();
-    widget.store.getSyntomsTypes();
   }
 
-  dispose() {
-    super.dispose();
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _syntomStore = Provider.of<SyntomStore>(context);
+
+    // check to see if already called api
+    if (!_syntomStore.loading) {
+      _syntomStore.getSyntoms();
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Observer(builder: (context) {
-        print('error ${widget.store.error}');
-        print('success ${widget.store.success}');
-        print('loading ${widget.store.loading}');
-        print('list ${widget.store.syntomsTypes}');
-        if (widget.store.loading) {
+        if (_syntomStore.loading) {
           return LoadingWidget();
         }
 
-        if (widget.store.error) {
+        if (_syntomStore.error) {
           return Center(
             child: Column(
               children: <Widget>[
@@ -51,7 +55,7 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 Text(
-                  'Por favor, tente novamente mais tarde.',
+                  'Por favor, tente novamente mais tarde!',
                 ),
                 RaisedButton(
                   child: Text('Tentar novamente'),
@@ -61,19 +65,19 @@ class _HomeScreenState extends State<HomeScreen> {
           );
         }
 
-        if (widget.store.success &&
-            widget.store.syntomsTypes != null &&
-            widget.store.syntomsTypes.isNotEmpty) {
+        if (_syntomStore.success &&
+            _syntomStore.syntoms != null &&
+            _syntomStore.syntoms.isNotEmpty) {
           return ListView.builder(
-            itemCount: widget.store.syntomsTypes.length,
+            itemCount: _syntomStore.syntoms.length,
             itemBuilder: (context, index) {
-              return Text(widget.store.syntomsTypes[index].name);
+              return Text(_syntomStore.syntoms[index].name);
             },
           );
         }
 
-        if (widget.store.success && widget.store.syntomsTypes.isEmpty ||
-            widget.store.syntomsTypes == null) {
+        if (_syntomStore.success && _syntomStore.syntoms.isEmpty ||
+            _syntomStore.syntoms == null) {
           return Center(
             child: Text(
               'Sem nada ainda',
