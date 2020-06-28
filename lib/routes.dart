0573d56c6b1dda69ba5f7ef83330f7672eded4cd
Larissa <larissasilvavarjao@gmail.com>
2020-06-28 12:21:12 -0300

    feat: add login and register routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sintomed_app/src/ui/screens/index.dart';
 
 import 'package:sintomed_app/src/ui/screens/login_screen.dart';
 import 'package:sintomed_app/src/ui/screens/private_app_screen.dart';
@@ -8,15 +9,17 @@ import 'package:sintomed_app/src/ui/screens/splash_screen.dart';
 class Routes {
   Routes._();
 
-  static const String splash = '/splash';
-  static const String login = '/login';
   static const String home = '/home';
+  static const String login = '/login';
+  static const String register = '/register';
+  static const String splash = '/splash';
   static const String welcome = '/welcome';
 
   static final routes = <String, WidgetBuilder>{
-    splash: (BuildContext context) => SplashScreen(),
-    login: (BuildContext context) => LoginScreen(),
     home: (BuildContext context) => PrivateAppScreen(),
+    login: (BuildContext context) => LoginScreen(),
+    splash: (BuildContext context) => SplashScreen(),
     welcome: (BuildContext context) => WelcomeScreen(),
+    register: (BuildContext context) => RegisterScreen(),
   };
 }
