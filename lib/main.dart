b0bbdacdf82c2c451f647cd5260e64a85176019a
Larissa <larissasilvavarjao@gmail.com>
2020-06-28 12:20:59 -0300

    feat: add userStore to provider

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:sintomed_app/routes.dart';
 import 'package:sintomed_app/src/stores/syntom/syntom_store.dart';
+import 'package:sintomed_app/src/stores/user/user_store.dart';
 import 'package:sintomed_app/src/ui/screens/splash_screen.dart';
 import 'package:sintomed_app/src/utils/colors.dart';
 
@@ -12,12 +13,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   final SyntomStore _syntomStore = SyntomStore();
+  final UserStore _userStore = UserStore();
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider<SyntomStore>.value(value: _syntomStore),
+        Provider<UserStore>.value(value: _userStore),
       ],
       child: Observer(
         builder: (context) {
