c73671944f05c5799eae2b5b67a4a7a63d4189a2
Larissa <larissasilvavarjao@gmail.com>
2020-06-29 22:26:47 -0300

    feat: add provider to global state management

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,38 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
 import 'package:sintomed_app/routes.dart';
 import 'package:sintomed_app/src/stores/syntom/syntom_store.dart';
-import 'package:sintomed_app/src/stores/user/user_store.dart';
+import 'package:sintomed_app/src/stores/auth/auth_store.dart';
 import 'package:sintomed_app/src/ui/screens/splash_screen.dart';
 import 'package:sintomed_app/src/utils/colors.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   final SyntomStore _syntomStore = SyntomStore();
-  final UserStore _userStore = UserStore();
+  final AuthStore _authStore = AuthStore();
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
         Provider<SyntomStore>.value(value: _syntomStore),
-        Provider<UserStore>.value(value: _userStore),
+        Provider<AuthStore>.value(value: _authStore),
       ],
-      child: Observer(
-        builder: (context) {
-          return MaterialApp(
-            title: 'Sintomed',
-            routes: Routes.routes,
-            theme: ThemeData(
-              primaryColor: kPrimaryColor.shade900,
-              fontFamily: 'Montserrat',
-              primarySwatch: kPrimaryColor,
-              backgroundColor: Colors.white,
-              scaffoldBackgroundColor: Colors.white,
-            ),
-            home: SplashScreen(),
-          );
-        },
+      child: MaterialApp(
+        title: 'Sintomed',
+        routes: Routes.routes,
+        theme: ThemeData(
+          primaryColor: kPrimaryColor.shade900,
+          fontFamily: 'Montserrat',
+          primarySwatch: kPrimaryColor,
+          backgroundColor: Colors.white,
+          scaffoldBackgroundColor: Colors.white,
+        ),
+        home: SplashScreen(),
       ),
     );
   }
