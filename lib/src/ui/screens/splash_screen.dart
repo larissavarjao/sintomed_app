cac5cebbffe649af736b149a4b0bf9229b82341a
Larissa <larissasilvavarjao@gmail.com>
2020-06-29 22:30:17 -0300

    feat: adjust splach screen

--- lib/src/ui/screens/splash_screen.dart
+++ lib/src/ui/screens/splash_screen.dart
@@ -1,7 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sintomed_app/routes.dart';
+import 'package:sintomed_app/src/stores/auth/auth_store.dart';
 import 'package:sintomed_app/src/ui/widgets/loading_widget.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -10,10 +12,19 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  AuthStore _authStore;
+
   @override
   void initState() {
     super.initState();
     startTimer();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _authStore = Provider.of<AuthStore>(context);
   }
 
   @override
@@ -26,14 +37,13 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 
   startTimer() {
-    var _duration = Duration(milliseconds: 5000);
+    var _duration = Duration(milliseconds: 2000);
     return Timer(_duration, navigate);
   }
 
-  navigate() {
-    bool isLoggedIn = true;
-
-    if (isLoggedIn) {
+  navigate() async {
+    bool isLogged = await _authStore.isLoggedIn();
+    if (isLogged) {
       Navigator.of(context).pushReplacementNamed(Routes.home);
     } else {
       Navigator.of(context).pushReplacementNamed(Routes.welcome);
