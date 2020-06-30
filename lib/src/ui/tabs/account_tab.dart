d349bf1fa8fc3d7b688bea9dfb0b98fa427bd58c
Larissa <larissasilvavarjao@gmail.com>
2020-06-29 22:30:26 -0300

    feat: make draft logout

--- lib/src/ui/tabs/account_tab.dart
+++ lib/src/ui/tabs/account_tab.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:sintomed_app/routes.dart';
+import 'package:sintomed_app/src/stores/auth/auth_store.dart';
 
-class AccountTab extends StatelessWidget {
+class AccountTab extends StatefulWidget {
+  @override
+  _AccountTabState createState() => _AccountTabState();
+}
+
+class _AccountTabState extends State<AccountTab> {
+  AuthStore _authStore;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    _authStore = Provider.of<AuthStore>(context);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
           child: RaisedButton(
-            onPressed: () {},
+            onPressed: () async {
+              await _authStore.logOutUser();
+              Navigator.of(context).pushReplacementNamed(Routes.splash);
+            },
             child: Text('Sair'),
           ),
         ),
