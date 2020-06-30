981d399fe6819f1a101b7dbc39b3da43bf8f5f1e
Larissa <larissasilvavarjao@gmail.com>
2020-06-29 22:26:27 -0300

    feat: create auth store

---
+++ lib/src/stores/auth/auth_store.dart
@@ -0,0 +1,169 @@
+import 'package:dio/dio.dart';
+import 'package:mobx/mobx.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:sintomed_app/src/models/user_model.dart';
+import 'package:sintomed_app/src/models/user_token_model.dart';
+import 'package:sintomed_app/src/repositories/user_repository.dart';
+import 'package:sintomed_app/src/services/shared_pref_service.dart';
+
+part 'auth_store.g.dart';
+
+class AuthStore = _AuthStoreBase with _$AuthStore;
+
+abstract class _AuthStoreBase with Store {
+  UserRepository _repository = UserRepository();
+  SharedPrefService _prefService =
+      SharedPrefService(SharedPreferences.getInstance());
+
+  @observable
+  User user;
+  @observable
+  String token;
+
+  @observable
+  String firstName;
+  @observable
+  String lastName;
+  @observable
+  String pacientName;
+  @observable
+  String email;
+  @observable
+  String password;
+  @observable
+  String confirmPassword;
+
+  @observable
+  ObservableFuture<Response> registerUserFuture =
+      ObservableFuture<Response>(emptyRegisterUserResponse);
+
+  static ObservableFuture<Response> emptyRegisterUserResponse =
+      ObservableFuture.value(null);
+
+  @observable
+  ObservableFuture<UserToken> loginUserFuture =
+      ObservableFuture<UserToken>(emptyLoginUserResponse);
+
+  static ObservableFuture<UserToken> emptyLoginUserResponse =
+      ObservableFuture.value(null);
+
+  @computed
+  bool get loading => registerUserFuture.status == FutureStatus.pending;
+
+  @computed
+  bool get error => registerUserFuture.status == FutureStatus.rejected;
+
+  @computed
+  bool get success => registerUserFuture.status == FutureStatus.fulfilled;
+
+  @computed
+  bool get isPasswordsEquals => this.password == this.confirmPassword;
+
+  @action
+  Future onChangeFirstName(String firstName) async {
+    this.firstName = firstName;
+  }
+
+  @action
+  Future<bool> isLoggedIn() async {
+    bool isLogged = false;
+    bool isLoggedOnStore = this.token != null;
+    bool isLoggedOnSharedPref = await _prefService.isLoggedIn;
+    print('isLoggedOnStore $isLoggedOnStore');
+    print('isLoggedOnSharedPref $isLoggedOnSharedPref');
+
+    if (!isLoggedOnStore && isLoggedOnSharedPref) {
+      String token = await _prefService.authToken;
+      print('prefToken $token');
+      this.token = token;
+      isLogged = true;
+    } else if (isLoggedOnStore && !isLoggedOnSharedPref) {
+      await _prefService.saveAuthToken(this.token);
+      isLogged = true;
+    } else if (isLoggedOnStore && isLoggedOnSharedPref) {
+      isLogged = true;
+    }
+
+    return isLogged;
+  }
+
+  @action
+  Future setToken(String token) async {
+    await _prefService.saveAuthToken(token);
+    this.token = token;
+  }
+
+  @action
+  Future onChangeLastName(String lastName) async {
+    this.lastName = lastName;
+  }
+
+  @action
+  Future onChangePacienttName(String pacientName) async {
+    this.pacientName = pacientName;
+  }
+
+  @action
+  Future onChangeEmail(String email) async {
+    this.email = email.trim();
+  }
+
+  @action
+  Future onChangePassword(String password) async {
+    this.password = password.trim();
+  }
+
+  @action
+  Future onChangeConfirmPassword(String confirmPassword) async {
+    this.confirmPassword = confirmPassword.trim();
+  }
+
+  @action
+  Future<bool> registerUser() async {
+    final future = _repository.registerUser(this.email, this.password,
+        this.firstName, this.lastName, this.pacientName);
+    registerUserFuture = ObservableFuture(future);
+
+    bool isUserRegistered;
+
+    await future.then((newUser) {
+      isUserRegistered = true;
+    }).catchError((error) => isUserRegistered = false);
+
+    return isUserRegistered;
+  }
+
+  @action
+  Future<bool> logInUser() async {
+    final future = _repository.loginUser(this.email, this.password);
+    loginUserFuture = ObservableFuture(loginUserFuture);
+
+    bool isUserLogged;
+
+    await future.then((userToken) {
+      this.user = User.fromJson(userToken['user']);
+      this.token = userToken['token'];
+      this.setToken(token);
+
+      isUserLogged = true;
+    }).catchError((error) => isUserLogged = false);
+
+    return isUserLogged;
+  }
+
+  @action
+  Future<void> logOutUser() async {
+    this.user = null;
+    this.token = null;
+    this.firstName = null;
+    this.lastName = null;
+    this.pacientName = null;
+    this.email = null;
+    this.password = null;
+    this.confirmPassword = null;
+
+    await _prefService.removeAuthToken();
+
+    print('isLoggedIn ${this.isLoggedIn}');
+  }
+}
--- lib/src/stores/auth/auth_store.g.dart
+++ lib/src/stores/auth/auth_store.g.dart
@@ -1,6 +1,6 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'user_store.dart';
+part of 'auth_store.dart';
 
 // **************************************************************************
 // StoreGenerator
@@ -8,34 +8,34 @@ part of 'user_store.dart';
 
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
-mixin _$UserStore on _UserStoreBase, Store {
+mixin _$AuthStore on _AuthStoreBase, Store {
   Computed<bool> _$loadingComputed;
 
   @override
   bool get loading => (_$loadingComputed ??=
-          Computed<bool>(() => super.loading, name: '_UserStoreBase.loading'))
+          Computed<bool>(() => super.loading, name: '_AuthStoreBase.loading'))
       .value;
   Computed<bool> _$errorComputed;
 
   @override
   bool get error => (_$errorComputed ??=
-          Computed<bool>(() => super.error, name: '_UserStoreBase.error'))
+          Computed<bool>(() => super.error, name: '_AuthStoreBase.error'))
       .value;
   Computed<bool> _$successComputed;
 
   @override
   bool get success => (_$successComputed ??=
-          Computed<bool>(() => super.success, name: '_UserStoreBase.success'))
+          Computed<bool>(() => super.success, name: '_AuthStoreBase.success'))
       .value;
   Computed<bool> _$isPasswordsEqualsComputed;
 
   @override
   bool get isPasswordsEquals => (_$isPasswordsEqualsComputed ??= Computed<bool>(
           () => super.isPasswordsEquals,
-          name: '_UserStoreBase.isPasswordsEquals'))
+          name: '_AuthStoreBase.isPasswordsEquals'))
       .value;
 
-  final _$userAtom = Atom(name: '_UserStoreBase.user');
+  final _$userAtom = Atom(name: '_AuthStoreBase.user');
 
   @override
   User get user {
@@ -50,7 +50,22 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$firstNameAtom = Atom(name: '_UserStoreBase.firstName');
+  final _$tokenAtom = Atom(name: '_AuthStoreBase.token');
+
+  @override
+  String get token {
+    _$tokenAtom.reportRead();
+    return super.token;
+  }
+
+  @override
+  set token(String value) {
+    _$tokenAtom.reportWrite(value, super.token, () {
+      super.token = value;
+    });
+  }
+
+  final _$firstNameAtom = Atom(name: '_AuthStoreBase.firstName');
 
   @override
   String get firstName {
@@ -65,7 +80,7 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$lastNameAtom = Atom(name: '_UserStoreBase.lastName');
+  final _$lastNameAtom = Atom(name: '_AuthStoreBase.lastName');
 
   @override
   String get lastName {
@@ -80,7 +95,7 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$pacientNameAtom = Atom(name: '_UserStoreBase.pacientName');
+  final _$pacientNameAtom = Atom(name: '_AuthStoreBase.pacientName');
 
   @override
   String get pacientName {
@@ -95,7 +110,7 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$emailAtom = Atom(name: '_UserStoreBase.email');
+  final _$emailAtom = Atom(name: '_AuthStoreBase.email');
 
   @override
   String get email {
@@ -110,7 +125,7 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$passwordAtom = Atom(name: '_UserStoreBase.password');
+  final _$passwordAtom = Atom(name: '_AuthStoreBase.password');
 
   @override
   String get password {
@@ -125,7 +140,7 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$confirmPasswordAtom = Atom(name: '_UserStoreBase.confirmPassword');
+  final _$confirmPasswordAtom = Atom(name: '_AuthStoreBase.confirmPassword');
 
   @override
   String get confirmPassword {
@@ -140,25 +155,39 @@ mixin _$UserStore on _UserStoreBase, Store {
     });
   }
 
-  final _$registerUserTypesFutureAtom =
-      Atom(name: '_UserStoreBase.registerUserTypesFuture');
+  final _$registerUserFutureAtom =
+      Atom(name: '_AuthStoreBase.registerUserFuture');
 
   @override
-  ObservableFuture<Response<dynamic>> get registerUserTypesFuture {
-    _$registerUserTypesFutureAtom.reportRead();
-    return super.registerUserTypesFuture;
+  ObservableFuture<Response<dynamic>> get registerUserFuture {
+    _$registerUserFutureAtom.reportRead();
+    return super.registerUserFuture;
   }
 
   @override
-  set registerUserTypesFuture(ObservableFuture<Response<dynamic>> value) {
-    _$registerUserTypesFutureAtom
-        .reportWrite(value, super.registerUserTypesFuture, () {
-      super.registerUserTypesFuture = value;
+  set registerUserFuture(ObservableFuture<Response<dynamic>> value) {
+    _$registerUserFutureAtom.reportWrite(value, super.registerUserFuture, () {
+      super.registerUserFuture = value;
+    });
+  }
+
+  final _$loginUserFutureAtom = Atom(name: '_AuthStoreBase.loginUserFuture');
+
+  @override
+  ObservableFuture<UserToken> get loginUserFuture {
+    _$loginUserFutureAtom.reportRead();
+    return super.loginUserFuture;
+  }
+
+  @override
+  set loginUserFuture(ObservableFuture<UserToken> value) {
+    _$loginUserFutureAtom.reportWrite(value, super.loginUserFuture, () {
+      super.loginUserFuture = value;
     });
   }
 
   final _$onChangeFirstNameAsyncAction =
-      AsyncAction('_UserStoreBase.onChangeFirstName');
+      AsyncAction('_AuthStoreBase.onChangeFirstName');
 
   @override
   Future<dynamic> onChangeFirstName(String firstName) {
@@ -166,8 +195,22 @@ mixin _$UserStore on _UserStoreBase, Store {
         .run(() => super.onChangeFirstName(firstName));
   }
 
+  final _$isLoggedInAsyncAction = AsyncAction('_AuthStoreBase.isLoggedIn');
+
+  @override
+  Future<bool> isLoggedIn() {
+    return _$isLoggedInAsyncAction.run(() => super.isLoggedIn());
+  }
+
+  final _$setTokenAsyncAction = AsyncAction('_AuthStoreBase.setToken');
+
+  @override
+  Future<dynamic> setToken(String token) {
+    return _$setTokenAsyncAction.run(() => super.setToken(token));
+  }
+
   final _$onChangeLastNameAsyncAction =
-      AsyncAction('_UserStoreBase.onChangeLastName');
+      AsyncAction('_AuthStoreBase.onChangeLastName');
 
   @override
   Future<dynamic> onChangeLastName(String lastName) {
@@ -176,7 +219,7 @@ mixin _$UserStore on _UserStoreBase, Store {
   }
 
   final _$onChangePacienttNameAsyncAction =
-      AsyncAction('_UserStoreBase.onChangePacienttName');
+      AsyncAction('_AuthStoreBase.onChangePacienttName');
 
   @override
   Future<dynamic> onChangePacienttName(String pacientName) {
@@ -185,7 +228,7 @@ mixin _$UserStore on _UserStoreBase, Store {
   }
 
   final _$onChangeEmailAsyncAction =
-      AsyncAction('_UserStoreBase.onChangeEmail');
+      AsyncAction('_AuthStoreBase.onChangeEmail');
 
   @override
   Future<dynamic> onChangeEmail(String email) {
@@ -193,7 +236,7 @@ mixin _$UserStore on _UserStoreBase, Store {
   }
 
   final _$onChangePasswordAsyncAction =
-      AsyncAction('_UserStoreBase.onChangePassword');
+      AsyncAction('_AuthStoreBase.onChangePassword');
 
   @override
   Future<dynamic> onChangePassword(String password) {
@@ -202,7 +245,7 @@ mixin _$UserStore on _UserStoreBase, Store {
   }
 
   final _$onChangeConfirmPasswordAsyncAction =
-      AsyncAction('_UserStoreBase.onChangeConfirmPassword');
+      AsyncAction('_AuthStoreBase.onChangeConfirmPassword');
 
   @override
   Future<dynamic> onChangeConfirmPassword(String confirmPassword) {
@@ -210,24 +253,40 @@ mixin _$UserStore on _UserStoreBase, Store {
         .run(() => super.onChangeConfirmPassword(confirmPassword));
   }
 
-  final _$registerUserAsyncAction = AsyncAction('_UserStoreBase.registerUser');
+  final _$registerUserAsyncAction = AsyncAction('_AuthStoreBase.registerUser');
 
   @override
   Future<bool> registerUser() {
     return _$registerUserAsyncAction.run(() => super.registerUser());
   }
 
+  final _$logInUserAsyncAction = AsyncAction('_AuthStoreBase.logInUser');
+
+  @override
+  Future<bool> logInUser() {
+    return _$logInUserAsyncAction.run(() => super.logInUser());
+  }
+
+  final _$logOutUserAsyncAction = AsyncAction('_AuthStoreBase.logOutUser');
+
+  @override
+  Future<void> logOutUser() {
+    return _$logOutUserAsyncAction.run(() => super.logOutUser());
+  }
+
   @override
   String toString() {
     return '''
 user: ${user},
+token: ${token},
 firstName: ${firstName},
 lastName: ${lastName},
 pacientName: ${pacientName},
 email: ${email},
 password: ${password},
 confirmPassword: ${confirmPassword},
-registerUserTypesFuture: ${registerUserTypesFuture},
+registerUserFuture: ${registerUserFuture},
+loginUserFuture: ${loginUserFuture},
 loading: ${loading},
 error: ${error},
 success: ${success},
