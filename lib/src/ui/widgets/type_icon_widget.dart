75aee845e977585e383c52b4d973bbd28a5ec106
Larissa Varjão <larissavarjao@Larissas-MacBook-Pro.local>
2020-07-17 21:10:04 -0300

    feat: add type icon widget

---
+++ lib/src/ui/widgets/type_icon_widget.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:sintomed_app/src/models/symptom_model.dart';
+import 'package:sintomed_app/src/ui/widgets/button_widget.dart';
+
+class TypeIconWidget extends StatelessWidget {
+  final Symptom symptom;
+
+  TypeIconWidget({
+    this.symptom,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    print('${symptom.typeName}');
+    return Hero(
+      tag: 'type_icon${symptom.id}',
+      child: ClipRRect(
+        borderRadius: BorderRadius.all(
+          Radius.circular(kBorderRadiusButton),
+        ),
+        child: Image.asset(
+          'images/${symptom.typeName.toLowerCase()}-square.png',
+          height: 50.0,
+        ),
+      ),
+    );
+  }
+}
